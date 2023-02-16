55cd01d6002c772f424022708d16a6675747c5c4
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-16 09:46:51 -0500

    app.js modified but will change in the future

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Register from "./pages/Register";
 function App() {
-  return <div>osman</div>;
+  return;
+  <Router>
+    <Switch>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
