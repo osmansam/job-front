81fc7dc6c36cefb8e2095598fb826c3497f7c38c
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-16 09:19:23 -0500

    gecici ekleme

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,6 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import { createJobThunk, deleteJobThunk, editJobThunk } from "./jobThunk";
+import {
+  createJobThunk,
+  deleteJobThunk,
+  editJobThunk,
+  getAllJobsThunk,
+} from "./jobThunk";
 const initialState = {
   isLoading: false,
   position: "",
@@ -16,6 +21,8 @@ const initialState = {
 const createJob = createAsyncThunk("job/create", createJobThunk);
 const deleteJob = createAsyncThunk("job/delete", deleteJobThunk);
 const editJob = createAsyncThunk("job/edit", editJobThunk);
+//bu gecici olarak burada
+const getAllJobs = createAsyncThunk("job/getAll", getAllJobsThunk);
 
 export const jobSlice = createSlice({
   name: "job",
@@ -60,6 +67,17 @@ export const jobSlice = createSlice({
       toast.success("Job updated successfully.");
     },
     [editJob.rejected]: (state, action) => {
+      state.isLoading = false;
+      toast.error(action.payload);
+    },
+    //bu gecici olarak burada
+    [getAllJobs.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getAllJobs.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [getAllJobs.rejected]: (state, action) => {
       state.isLoading = false;
       toast.error(action.payload);
     },
