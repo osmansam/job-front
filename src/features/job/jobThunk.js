1d60705787c668ad5add15b957ca007a8a904b0f
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-15 22:21:44 -0500

    job slice and job thunk are mainly created

---
+++ src/features/job/jobThunk.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { checkForUnauthorizedResponse } from "../../utils/axios";
+import { clearValues } from "./jobSlice";
+
+const baseURL = "/api/v1";
+export const createJobThunk = async (job, thunkAPI) => {
+  try {
+    const resp = await axios.post(`${baseURL}/job`, job);
+    thunkAPI.dispatch(clearValues());
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
+export const deleteJobThunk = async (id, thunkAPI) => {
+  try {
+    const resp = await axios.delete(`${baseURL}/job/${id}`);
+    thunkAPI.dispatch(clearValues());
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
+export const updateJobThunk = async (id, job, thunkAPI) => {
+  try {
+    const resp = await axios.patch(`${baseURL}/job/${id}`, job);
+    thunkAPI.dispatch(clearValues());
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
