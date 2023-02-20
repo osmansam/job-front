5d175b74938f6804e104279fdde691946d3fca64
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-19 21:35:26 -0500

    form row styling done

--- src/components/FormRowSelect.js
+++ src/components/FormRowSelect.js
@@ -1,25 +1,49 @@
+import styled from "styled-components";
+
 const FormRowSelect = ({ labelText, name, value, handleChange, list }) => {
   return (
-    <div className="form-row">
-      <label htmlFor={name} className="form-label">
-        {labelText || name}
-      </label>
-      <select
-        name={name}
-        id={name}
-        value={value}
-        onChange={handleChange}
-        className="form-select"
-      >
-        {list.map((itemValue, index) => {
-          return (
-            <option key={index} value={itemValue}>
-              {itemValue}
-            </option>
-          );
-        })}
-      </select>
-    </div>
+    <Wrapper>
+      <div className="formrowselect">
+        <label htmlFor={name} className="form-label">
+          {labelText || name}
+        </label>
+        <select
+          name={name}
+          id={name}
+          value={value}
+          onChange={handleChange}
+          className="form-select"
+        >
+          {list.map((itemValue, index) => {
+            return (
+              <option key={index} value={itemValue}>
+                {itemValue}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  .formrowselect {
+    margin-bottom: 0.5rem;
+  }
+  .form-label {
+    display: block;
+    margin-bottom: 0.5rem;
+    text-transform: capitalize;
+  }
+  .form-select {
+    width: 100%;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    margin-left: 0.5rem;
+    text-transform: capitalize;
+  }
+`;
+
 export default FormRowSelect;
