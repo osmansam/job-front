a8ff15097407cfa6e447b4e0f03c21921a7df1d4
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-19 21:35:59 -0500

    form row styling

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,18 +1,38 @@
+import styled from "styled-components";
+
 const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
-    <div className="form-row">
-      <label htmlFor={name} className="form-label">
-        {labelText || name}
-      </label>
-      <input
-        id={name}
-        type={type}
-        name={name}
-        value={value}
-        onChange={handleChange}
-        className="form-input"
-      />
-    </div>
+    <Wrapper>
+      <div className="form-row">
+        <label htmlFor={name} className="form-label">
+          {labelText || name}
+        </label>
+        <input
+          id={name}
+          type={type}
+          name={name}
+          value={value}
+          onChange={handleChange}
+          className="form-input"
+        />
+      </div>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  .form-row {
+    margin-bottom: 1rem;
+  }
+  .form-label {
+    display: block;
+    margin-bottom: 0.5rem;
+    text-transform: capitalize;
+  }
+  .form-input {
+    width: 100%;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+  }
+`;
 export default FormRow;
