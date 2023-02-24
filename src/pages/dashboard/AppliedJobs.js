68cc3b5d74e8a9887a16e665958561dd51d92e71
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-24 10:12:53 -0500

    applied job page buttons are added functionality still dont work

--- src/pages/dashboard/AppliedJobs.js
+++ src/pages/dashboard/AppliedJobs.js
@@ -3,20 +3,24 @@ import { useDispatch, useSelector } from "react-redux";
 import JobContainer from "../../components/JobContainer";
 import styled from "styled-components";
 import { getAllCandidates } from "../../features/user/userSlice";
+import { changeAppliedJobPage } from "../../features/search/searchSlice";
 const AppliedJobs = () => {
   const { user, candidates } = useSelector((state) => state.user);
   const { jobs } = useSelector((state) => state.search);
-  const [appliedJobs, setAppliedJobs] = useState([]);
+  const [numberOfPages, setNumberOfPages] = useState(0);
   const dispatch = useDispatch();
-
+  const { appliedJobspage } = useSelector((state) => state.search);
+  const handlePageChange = (pageNumber) => {
+    dispatch(changeAppliedJobPage(pageNumber));
+  };
   useEffect(() => {
     dispatch(getAllCandidates());
     const appliedJobs = candidates?.filter((candidate) => {
       return candidate.user === user.userId;
     });
     const jobs = appliedJobs?.map((obj) => obj.job);
-    console.log(jobs);
-  }, []);
+    setNumberOfPages(Math.ceil(jobs.length / 4));
+  }, [user]);
 
   return (
     <Wrapper>
@@ -24,8 +28,58 @@ const AppliedJobs = () => {
       <div className="jobs-container">
         <JobContainer jobs={jobs} />
       </div>
+      <div className="page-container">
+        {numberOfPages > 1 &&
+          Array.from({ length: numberOfPages }, (_, i) => {
+            const pageNumber = i + 1;
+            return (
+              <button
+                key={pageNumber}
+                className={`${
+                  appliedJobspage === pageNumber ? "active" : null
+                }`}
+                onClick={() => {
+                  handlePageChange(pageNumber);
+                }}
+              >
+                {pageNumber}
+              </button>
+            );
+          })}
+      </div>
     </Wrapper>
   );
 };
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100%;
+  display: inline-block;
+  .title {
+    width: 100%;
+    font-size: 2rem;
+    font-weight: 700;
+    text-transform: capitalize;
+    margin: 3rem auto;
+    padding-left: 3rem;
+    border-bottom: 1px solid #ccc;
+  }
+  .jobs-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1.5rem;
+  }
+
+  .page-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1.5rem;
+  }
+  .active {
+    background-color: black;
+    color: #fff;
+  }
+`;
 export default AppliedJobs;
